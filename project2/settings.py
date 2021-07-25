a00f28430dcd46108475ac60e70d33282402af19
WassupAditya <aditya249645@gmail.com>
2021-07-25 13:45:12 +0530

    updated

--- project2/settings.py
+++ project2/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+DEBUG = True
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_8pnq@j=rc#(@j+rq%d%5&&kild07=@dx__g@%wm@tus58f$5f'
 
@@ -122,9 +124,10 @@ STATIC_URL = '/static/'
 
 #Added manually
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR , "static"),
     
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
